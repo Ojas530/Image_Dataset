960e575c8b0ead984f15277022ae81c74932326e
Ojas530 <ojasahirrao530@gmail.com>
2020-10-10 10:00:15 +0530

    Burger Images

--- images.py
+++ images.py
@@ -3,6 +3,7 @@ images = input("are there any images?")
 if images == "yes":
     print("Good quality images")
     print("Images in the png format")
+    print("Download Burger Images")
 else:
     print("Download it form the codebasics dataset")
 
