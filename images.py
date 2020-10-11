5411ff12cb06ceabbf48e18f5bab611c7b930e64
Ojas530 <ojasahirrao530@gmail.com>
2020-10-11 09:04:50 +0530

    YouTube Videos

--- images.py
+++ images.py
@@ -4,6 +4,7 @@ if images == "yes":
     print("Good quality images")
     print("Images in the png format")
     print("Download Burger Images")
+    print("Download videos for Blogs")
 else:
     print("Download it form the codebasics dataset")
 
